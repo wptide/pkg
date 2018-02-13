601b0ae1b37702a907915a93219132551b480590
Rheinard Korf <rheinard@thekorfs.com>
2018-02-13 12:02:10 +1100

    Allow logger to SetOutput.

--- log/log.go
+++ log/log.go
@@ -3,10 +3,15 @@ package log
 import (
 	"log"
 	"os"
+	"io"
 )
 
 var logger = log.New(os.Stdout,"", log.Ldate | log.Ltime )
 
 func Log(title interface{}, content interface{}) {
 	logger.Printf("%s: %s", title, content)
+}
+
+func SetOutput(w io.Writer) {
+	logger.SetOutput(w)
 }
